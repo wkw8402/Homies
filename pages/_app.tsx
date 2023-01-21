0accef8a25cecb9578f9216dec6baa87f96a3671
Alec Dewitz <atdewitz@gmail.com>
2023-01-20 19:36:54 -0600

    Add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
+import Script from 'next/script';
 import '../styles/globals.scss';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      {process.env.NODE_ENV === 'production' && (
+        <>
+          <Script
+            strategy="afterInteractive"
+            src="https://www.googletagmanager.com/gtag/js?id=G-J1Z72RYK2X"
+          ></Script>
+          <Script id="google-analytics" strategy="afterInteractive">
+            {` window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          
+          gtag('config', 'G-J1Z72RYK2X');
+          `}
+          </Script>
+        </>
+      )}
+
       <Component {...pageProps} />
     </>
   );
