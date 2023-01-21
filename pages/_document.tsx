2d5dbc2ef218fed0d32dfac4f93fc3d94ee9fa7e
Alec Dewitz <atdewitz@gmail.com>
2023-01-20 18:36:18 -0600

    Added favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,93 +5,35 @@ export default function Document() {
     <Html>
       <Head>
         <meta charSet="UTF-8" />
-        <link rel="shortcut icon" href="images/favicons/favicon.ico" />
+        <link rel="shortcut icon" href="/favicon.ico" />
         <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="images/favicons/favicon-16x16.png"
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="images/favicons/favicon-32x32.png"
+          href="/favicon-32x32.png"
         />
         <link
           rel="icon"
           type="image/png"
-          sizes="48x48"
-          href="images/favicons/favicon-48x48.png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
         />
-        <link rel="manifest" href="images/favicons/manifest.json" />
+        <link rel="manifest" href="/site.webmanifest"></link>
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="theme-color" content="#fff" />
-        <meta name="application-name" content="Tailwind Webpack Boilerplate" />
-        <link
-          rel="apple-touch-icon"
-          sizes="57x57"
-          href="images/favicons/apple-touch-icon-57x57.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="60x60"
-          href="images/favicons/apple-touch-icon-60x60.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="72x72"
-          href="images/favicons/apple-touch-icon-72x72.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="76x76"
-          href="images/favicons/apple-touch-icon-76x76.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="114x114"
-          href="images/favicons/apple-touch-icon-114x114.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="120x120"
-          href="images/favicons/apple-touch-icon-120x120.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="144x144"
-          href="images/favicons/apple-touch-icon-144x144.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="152x152"
-          href="images/favicons/apple-touch-icon-152x152.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="167x167"
-          href="images/favicons/apple-touch-icon-167x167.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="images/favicons/apple-touch-icon-180x180.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="1024x1024"
-          href="images/favicons/apple-touch-icon-1024x1024.png"
-        />
+        <meta name="application-name" content="Homies" />
+
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta
           name="apple-mobile-web-app-status-bar-style"
           content="black-translucent"
         />
-        <meta
-          name="apple-mobile-web-app-title"
-          content="Tailwind Webpack Boilerplate"
-        />
+        <meta name="apple-mobile-web-app-title" content="Homies" />
       </Head>
       <body>
         <Main />
