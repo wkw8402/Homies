285d6725f2f10df9c9a53d9faba4390a4be19df6
alecdewitz <13428171+alecdewitz@users.noreply.github.com>
2023-04-26 14:44:41 -0700

    Fix line height issues

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -8,29 +8,7 @@ import { getDatabase, getPropertyText } from '../../lib/notion';
 
 export const databaseId = process.env.NOTION_DATABASE_ID;
 
-const posts = [
-  {
-    id: 1,
-    title: 'Boost your conversion rate',
-    href: '#',
-    description:
-      'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel iusto corrupti dicta laboris incididunt.',
-    date: 'Mar 16, 2020',
-    datetime: '2020-03-16',
-    category: { title: 'Marketing', href: '#' },
-    author: {
-      name: 'Michael Foster',
-      role: 'Co-Founder / CTO',
-      href: '#',
-      imageUrl:
-        'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-  },
-  // More posts...
-];
-
 export default function Home({ posts }) {
-  console.log(posts);
   return (
     <>
       <Head>
@@ -75,7 +53,7 @@ export default function Home({ posts }) {
                       </a> */}
                     </div>
                     <div className="relative group">
-                      <h3 className="mt-3 text-4xl font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                      <h3 className="mt-3 text-2xl font-semibold leading-snug text-gray-900 sm:text-4xl group-hover:text-gray-600">
                         <Link
                           href={`/blog/${getPropertyText(
                             post.properties.Slug
