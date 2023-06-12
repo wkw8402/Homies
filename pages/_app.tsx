e4c31ea59ec95b5bba9162c51af1eb47d00d9f6e
alecdewitz <13428171+alecdewitz@users.noreply.github.com>
2023-06-11 23:10:07 -0500

    Add tracking code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -31,6 +31,13 @@ export default function App({
             defer
             src="//js-na1.hs-scripts.com/23979052.js"
           />
+          <Script id="clarity" strategy="afterInteractive">
+            {`(function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "hijoq9lp8j");`}
+          </Script>
         </>
       )}
 
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -41,13 +41,6 @@ export default function Document() {
           content="black-translucent"
         />
         <meta name="apple-mobile-web-app-title" content="Homies" />
-        <Script id="clarity">
-          {`(function(c,l,a,r,i,t,y){
-              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-          })(window, document, "clarity", "script", "hijoq9lp8j");`}
-        </Script>
       </Head>
 
       <body className="h-full">
