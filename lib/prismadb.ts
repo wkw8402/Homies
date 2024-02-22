9a7832ac3c92e3c1a517391c2f2dfef773437417
Alec Dewitz <atdewitz@gmail.com>
2024-02-22 11:49:49 -0800

    use prisma accelerate in prod and preview

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,13 +1,17 @@
 import { PrismaClient } from '@prisma/client';
+import { withAccelerate } from '@prisma/extension-accelerate';
 
 const globalForPrisma = global as unknown as {
-  prisma: PrismaClient | undefined;
+  prisma: any | undefined;
 };
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: ['query'],
-  });
+    log:
+      process.env.NODE_ENV !== 'production'
+        ? ['query', 'error', 'warn']
+        : ['error'],
+  }).$extends(withAccelerate());
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
