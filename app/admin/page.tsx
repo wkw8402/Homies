7c59aad9395feaac153718adddd49dc61b5e2bb5
Joshua Chen <joshua.chen79@gmail.com>
2024-04-11 00:01:21 -0700

    Added Back to Dashboard button on admin view page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,8 +1,10 @@
 import { authOptions } from '@/lib/auth';
 import { getServerSession } from 'next-auth';
 import Head from 'next/head';
+import Link from 'next/link';
 import { notFound } from 'next/navigation';
 import { prisma } from '../../lib/prismadb';
+import { ArrowLeftCircleIcon } from '@heroicons/react/24/outline';
 import AdminDashboard from './components/admin-dashboard';
 
 export const dynamic = 'force-dynamic';
@@ -50,6 +52,16 @@ const AdminPage = async () => {
       <Head>
         <title>Admin | Homies</title>
       </Head>
+      <div className="bg-gray-100">
+        <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between mb-4">
+            <Link className="inline-flex items-center p-3 sm:py-8" href="/dashboard">
+              <ArrowLeftCircleIcon className="w-8 h-8 mr-2" aria-hidden="true" />
+              Back to Dashboard
+            </Link>
+          </div>
+        </div>
+      </div>
       <AdminDashboard profiles={profiles} />
     </>
   );
