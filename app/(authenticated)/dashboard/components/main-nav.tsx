96582bd40930974f105458b50a0d494e4168f104
aidansunbury <aidansunbury@gmail.com>
2024-05-05 14:53:55 -0700

    added emergency contact to schema, and added settings to nav bar

--- app/(authenticated)/dashboard/components/main-nav.tsx
+++ app/(authenticated)/dashboard/components/main-nav.tsx
@@ -3,12 +3,12 @@
 import { cn } from '@/lib/utils';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
 export function MainNav(props) {
   const activePath = usePathname();
 
-  const [profileId, setProfileId] = useState("");
+  const [profileId, setProfileId] = useState('');
   const [isAdmin, setIsAdmin] = useState(false);
 
   useEffect(() => {
@@ -16,48 +16,49 @@ export function MainNav(props) {
       const userId = props.session?.user?.id;
 
       try {
-        const profileRes = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/user/profile/${userId}`, {
-          method: "GET",
-          headers: {
-            "Content-Type": "application.json"
+        const profileRes = await fetch(
+          `${process.env.NEXT_PUBLIC_BASE_URL}/api/user/profile/${userId}`,
+          {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application.json',
+            },
           }
-        })
+        );
 
         const profile = await profileRes.json();
         setProfileId(profile.id);
-
       } catch (error) {
         console.error('Error retrieving profile:', error);
       }
-    }
+    };
 
     const checkIsAdmin = async () => {
       const userId = props.session?.user?.id;
 
       try {
-        const userRes = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/user/${userId}`, {
-          method: "GET",
-          headers: {
-            "Content-Type": "application.json"
+        const userRes = await fetch(
+          `${process.env.NEXT_PUBLIC_BASE_URL}/api/user/${userId}`,
+          {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application.json',
+            },
           }
-        })
+        );
 
         const user = await userRes.json();
         setIsAdmin(user.admin);
-
       } catch (error) {
         console.error('Error retrieving profile:', error);
       }
-    }
+    };
     fetchProfile();
     checkIsAdmin();
-
-  }, [props.session?.user?.id])
+  }, [props.session?.user?.id]);
 
   return (
-    <nav
-      className={cn('flex items-center space-x-4 lg:space-x-6 mx-6')}
-    >
+    <nav className={cn('flex items-center space-x-4 lg:space-x-6 mx-6')}>
       {/* <Image alt="logo" src={'/images/logo.png'} width={80} height={50} /> */}
 
       <Link
@@ -101,6 +102,16 @@ export function MainNav(props) {
         Profile
       </Link>
       <Link
+        href={`/settings`}
+        className={cn(
+          activePath === '/settings' ? 'text-primary' : 'text-muted-foreground',
+          'text-sm font-medium transition-colors hover:text-primary'
+        )}
+        rel="noopener noreferrer"
+      >
+        Settings
+      </Link>
+      <Link
         href={`/profile/${profileId}`}
         className={cn(
           activePath === '/profile/share'
@@ -113,7 +124,7 @@ export function MainNav(props) {
       >
         Share
       </Link>
-      {isAdmin &&
+      {isAdmin && (
         <Link
           href="/admin"
           className={cn(
@@ -125,7 +136,7 @@ export function MainNav(props) {
         >
           Admin Dashboard
         </Link>
-      }
+      )}
     </nav>
   );
 }
